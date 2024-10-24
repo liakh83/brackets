c3adcd53bfde9cec44084de9e3011570725ed96d
liakh83 <lahn4573@gmail.com>
2024-10-24 21:34:27 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  
-}
+  // Pair bracket configurations and create a new array
+  let createPairs = bracketsConfig.map((pair) => pair.join(""));
+
+  //  Initialize the variable that will save the previous length of the string
+  let previousLength = -1;
+
+  //Find matches of brackets with pair and remove them from the string
+  while (str.length !== previousLength) {
+    previousLength = str.length;
+
+    createPairs.forEach((pair) => {
+      str = str.replace(pair, "");
+    });
+  }
+  // If the string is completely empty all brackets have been removed correctly
+  return str.length === 0;
+};
